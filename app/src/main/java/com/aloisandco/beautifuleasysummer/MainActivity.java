7508e4ce838db8e07354058a3f349c8cabf995ce
Quentin Metzler <quentin.metzler@gmail.com>
2015-03-15 01:13:13 +0100

    Add logo animation
    Add logo animation and fix flickering

--- app/src/main/java/com/aloisandco/beautifuleasysummer/MainActivity.java
+++ app/src/main/java/com/aloisandco/beautifuleasysummer/MainActivity.java
@@ -3,12 +3,17 @@ package com.aloisandco.beautifuleasysummer;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 import com.aloisandco.beautifuleasysummer.utils.ImageViewUtils;
 
 
 public class MainActivity extends Activity {
+
+    private boolean firstLaunch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +22,30 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onWindowFocusChanged(boolean focus) {
-        super.onWindowFocusChanged(focus);
-        ImageView backgroundView = (ImageView) findViewById(R.id.background);
-        ImageView circleView = (ImageView) findViewById(R.id.circle);
-        // Make a hole in the backgroundView bitmap of the size of the circleView
-        Bitmap backgroundBitmap = ImageViewUtils.createCircleInImageViewFromSizeOfOtherImageView(backgroundView, circleView, getWindow());
-        // Set the new bitmap with the hole as the new background bitmap
-        backgroundView.setImageBitmap(backgroundBitmap);
+    protected void onResume() {
+        super.onResume();
+
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean focus) {
+        super.onWindowFocusChanged(focus);
+        if (firstLaunch) {
+            ImageView backgroundView = (ImageView) findViewById(R.id.background);
+            ImageView circleView = (ImageView) findViewById(R.id.circle);
+            // Make a hole in the backgroundView bitmap of the size of the circleView
+            Bitmap backgroundBitmap = ImageViewUtils.createCircleInImageViewFromSizeOfOtherImageView(backgroundView, circleView, getWindow());
+            // Set the new bitmap with the hole as the new background bitmap
+            backgroundView.setImageBitmap(backgroundBitmap);
 
-
-
+            ImageView logoView = (ImageView) findViewById(R.id.logo);
+            RotateAnimation anim = new RotateAnimation(0f, 360f, logoView.getWidth() / 2, circleView.getHeight());
+            anim.setInterpolator(new AccelerateDecelerateInterpolator());
+            anim.setRepeatCount(Animation.INFINITE);
+            anim.setStartOffset(3000);
+            anim.setDuration(2000);
+            logoView.startAnimation(anim);
+            firstLaunch = false;
+        }
+    }
 }
